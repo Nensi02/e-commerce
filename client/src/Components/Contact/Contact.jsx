854fd667da416036298783fd8ba168b5a564aa12
Darji Nensi <ndnensidarji@gmail.com>
2023-04-25 23:41:34 +0530

    Modified code

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GoogleIcon from '@mui/icons-material/Google';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import "./Contact.scss";
 
 export const Contact = () => {
   return (
